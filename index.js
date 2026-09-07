bca54352695031664999d32745a340c0598ce59a
jayTech78 <jayforex250@gmail.com>
2026-09-07 22:37:51 +0100

    index.js recorrrected

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const multer = require("multer");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-// Load environment variables
 dotenv.config();
 
-// Initialize Express
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 // Middleware
@@ -16,12 +15,13 @@ app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(express.json({ limit: "5mb" }));
 app.use(cors());
 
-// File Upload (multer)
+// File Upload
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
 const { upload_Image } = require("./controllers/upload-image.controllers");
 
-// Import Routes
+// Routes
 const parentRouter = require("./routes/parent.route");
 const studentRouter = require("./routes/student.route");
 const staffRouter = require("./routes/staff.route");
@@ -41,10 +41,11 @@ const auditRouter = require("./routes/audit.route");
 const termRouter = require("./routes/term.route");
 const sessionRouter = require("./routes/session.route");
 const resultRouter = require("./routes/result.route");
-const disciplinaryRouter = require("./routes/disciplinary.routes")
-const incomeRouter = require("./routes/income.route")
-const expenseRouter = require("./routes/expense.route")
-const cashbookRouter = require("./routes/cashbook.route")
+const disciplinaryRouter = require("./routes/disciplinary.routes");
+const incomeRouter = require("./routes/income.route");
+const expenseRouter = require("./routes/expense.route");
+const cashbookRouter = require("./routes/cashbook.route");
+
 require("./cron/promotionCron");
 
 // Routes
@@ -57,7 +58,9 @@ app.use("/manager", managerRouter);
 app.use("/payment", paymentRouter);
 app.use("/fees", feesRouter);
 app.use("/book", bookRouter);
+
 app.post("/upload-image", upload.single("file"), upload_Image);
+
 app.use("/role", roleRouter);
 app.use("/class", classRouter);
 app.use("/subject", subjectRouter);
@@ -69,21 +72,21 @@ app.use("/audit", auditRouter);
 app.use("/term", termRouter);
 app.use("/session", sessionRouter);
 app.use("/result", resultRouter);
-app.use("/income", incomeRouter)
-app.use("/expense", expenseRouter)
-app.use("/cashbook", cashbookRouter)
+app.use("/income", incomeRouter);
+app.use("/expense", expenseRouter);
+app.use("/cashbook", cashbookRouter);
 
 // MongoDB Connection
-// MongoDB Connection
-
-mongoose.connect(process.env.MONGO_URL)
+mongoose
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Mongo Connected Successfully");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`App is running on port ${PORT}`);
     });
   })
   .catch((err) => {
     console.error("There was a problem connecting to MongoDB:", err);
+    process.exit(1);
   });
